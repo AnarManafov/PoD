7f859bc448339020d20f97ba99e166d78f709e9e
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-02-27 18:53:09 +0000

    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/PROOFAgent/trunk@461 47ffe4a8-6206-0410-815e-8f101de37557

---
+++ PROOFAgent/src/proofagent.cpp
@@ -0,0 +1,29 @@
+/************************************************************************/
+/**
+ * @file proofagent.cpp
+ * @brief $$File comment$$
+ * @author Anar Manafov A.Manafov@gsi.de
+ *//*
+
+        version number:    $LastChangedRevision$
+        created by:        Anar Manafov
+                           $$date$$
+        last changed by:   $LastChangedBy$ $LastChangedDate$
+
+        Copyright (c) 2006,2007 GSI GridTeam. All rights reserved.
+*************************************************************************/
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  cout << "Hello, world!" << endl;
+
+  return EXIT_SUCCESS;
+}
