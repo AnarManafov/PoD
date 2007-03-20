dc777ce3b5a190a2dd31fa0b528a17b88a2c0b20
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-03-20 17:14:07 +0000

    changed: excluding MiscCommon Include from metrics of individual projects
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/PROOFAgent/trunk@592 47ffe4a8-6206-0410-815e-8f101de37557

--- PROOFAgent/sbin/gen_metrics.sh
+++ PROOFAgent/sbin/gen_metrics.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 echo ">>> producing log from SVN... <<<"
-
-echo '<?xml version="1.0"?>' > svn.log
-echo "<log>" >> svn.log
-svn log https://subversion.gsi.de/grid/D-Grid/Include --incremental --xml -v >> svn.log
-svn log https://subversion.gsi.de/grid/D-Grid/PROOFAgent --incremental --xml -v >> svn.log
-echo "</log>" >> svn.log
+svn log https://subversion.gsi.de/grid/D-Grid/PROOFAgent --xml -v > svn.log
 
 echo ">>> setting up DISPLAY... <<<"
 export DISPLAY=:0.0
@@ -16,11 +11,9 @@ echo ">>> pre-processing svn.log file... <<<"
 # We need to prepare output file for StatSVN in order to get tags.
 # This we need to do, since our project doesn't have dedicated repository in SVN and just is a part of "grid" repository
 sed -i "s/\/D-Grid\/PROOFAgent\/tags\//\/tags\//g" svn.log
-# adding external "Include" dir to the metrics
-sed -i "s/\/D-Grid\/Include/\/D-Grid\/PROOFAgent\/trunk\/PROOFAgent\/Include/g" svn.log
 
 echo ">>> executing StatSVN... <<<"
-java -jar trunk/PROOFAgent/sbin/statsvn.jar -include Include/** -css ./trunk/PROOFAgent/sbin/statsvn.css -output-dir ./PROOFAgent-metrics -tags '.*' svn.log .
+java -jar trunk/PROOFAgent/sbin/statsvn.jar -css ./trunk/PROOFAgent/sbin/statsvn.css -output-dir ./PROOFAgent-metrics -tags '.*' svn.log .
 
 
 echo ">>> generating SLOC count report... <<<"
