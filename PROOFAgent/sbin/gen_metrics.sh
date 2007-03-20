a583704b100dbfdcca164cbb13fc14e10d523d4d
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-03-20 16:35:48 +0000

    added: PROOFAgent has been added to autobuild process + metrics
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/PROOFAgent/trunk@590 47ffe4a8-6206-0410-815e-8f101de37557

--- PROOFAgent/sbin/gen_metrics.sh
+++ PROOFAgent/sbin/gen_metrics.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo ">>> producing log from SVN... <<<"
-svn log https://subversion.gsi.de/grid/D-Grid/PROOFAgent --xml -v > svn.log
+svn log https://subversion.gsi.de/grid/D-Grid/Include --xml -v > svn.log
+svn log https://subversion.gsi.de/grid/D-Grid/PROOFAgent --incremental --xml -v >> svn.log
 
 echo ">>> setting up DISPLAY... <<<"
 export DISPLAY=:0.0
@@ -22,6 +23,7 @@ echo ">>> generating SLOC count report... <<<"
 sloccount --crossdups  --wide ./trunk/PROOFAgent/ > result.txt
 ./trunk/PROOFAgent/sbin/sloc2html.py result.txt > ./PROOFAgent-metrics/result.html
 
-echo ">>> publishing metrics on the web... <<<" 
+echo ">>> publishing metrics on the web... <<<"
 chmod og+rx -R PROOFAgent-metrics
 scp -p -r PROOFAgent-metrics manafov@lxial24:/misc/manafov/web-docs/D-Grid/metrics
+
