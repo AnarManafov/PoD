ffb9f0716334a3249e74c33197f255183b4751b5
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-04-16 14:58:28 +0000

    added: simple test script for PROOFAgent
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/PROOFAgent/trunk@647 47ffe4a8-6206-0410-815e-8f101de37557

---
+++ PROOFAgent/sbin/proofagent.test.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+read3 ()                # Usage: read3
+{
+    read -r -u3              # read from fd=3
+}
+
+read4 ()
+{
+    read -r -u4   
+}
+
+send3 ()                # Usage: send3 [cmd...]
+{
+    echo "$*" 1>&3            # write to fd=3
+}
+
+send4 ()                # Usage: send4 [cmd...]
+{
+    echo "$*" 1>&4            # write to fd=4
+}
+
+end_session ()
+{
+   # shuting proofaget down
+    ./proofagent  --instance server --pidfile /tmp/ --stop
+    ./proofagent  --instance client1 --pidfile /tmp/ --stop
+    
+      # close file descriptors
+    exec 3<&-
+    exec 4<&-
+    
+    [[ "$*" == "OK" ]] || exit 1
+    exit 0
+}
+
+echo "Starting PROOFAgent Server:"
+./proofagent --config /home/anar/workspace/PROOFAgent/documentation/PROOFAgent_config/proofagent.cfg.xml  --instance server --pidfile /tmp/
+
+# Wait a bit untill proofagent fully loaded
+#sleep 3
+
+echo "Starting PROOFAgent Client: "
+./proofagent --config /home/anar/workspace/PROOFAgent/documentation/PROOFAgent_config/proofagent.cfg.xml  --instance client1 --pidfile /tmp/
+
+# Wait a bit untill proofagent fully loaded
+#sleep 3
+
+echo "processing socket tests..."
+exec 3<>/dev/tcp/127.0.0.1/20001 || end_session "ERROR";
+exec 4<>/dev/tcp/127.0.0.1/51511 || end_session "ERROR";
+
+
+send3 "HELLO TEST from SERVER";
+read4
+echo "$* --> $REPLY"
+
+[[ $REPLY == "HELLO TEST from SERVER" ]]  || end_session "ERROR";
+
+
+send4 "HELLO TEST from CLIENT";
+read3
+echo "$* --> $REPLY"
+[[ $REPLY == "HELLO TEST from CLIENT" ]]  || end_session "ERROR";
+
+
+end_session "OK";
