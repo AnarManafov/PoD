066e7d97b8f3d967f505e3a5fd2e3872d6d7f7db
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-04-18 16:03:39 +0000

    added: new option "daemonize" - run PROOFAgent as a daemon. Without this option PROOFAgent will run in interactive mode
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/PROOFAgent/trunk@670 47ffe4a8-6206-0410-815e-8f101de37557

--- PROOFAgent/src/Main.cpp
+++ PROOFAgent/src/Main.cpp
@@ -31,14 +31,25 @@ using namespace PROOFAgent;
 using namespace boost::program_options;
 
 // Function used to check that 'opt1' and 'opt2' are not specified at the same time.
-void conflicting_options(const variables_map& vm, const char* opt1, const char* opt2)
+void conflicting_options( const variables_map& _vm, const char* _opt1, const char* _opt2 )
 {
-    if (vm.count(opt1) && !vm[opt1].defaulted() && vm.count(opt2) && !vm[opt2].defaulted())
+    if ( _vm.count(_opt1) && !_vm[_opt1].defaulted() && _vm.count(_opt2) && !_vm[_opt2].defaulted() )
     {
         string str("Command line parameter \"%1\" conflicts with \"%2\""); // TODO: Move this message to the resource file
-        replace<string>( &str, "%1", opt1 );
-        replace<string>( &str, "%2", opt2 );
-        throw logic_error( str );
+        replace<string>( &str, "%1", _opt1 );
+        replace<string>( &str, "%2", _opt2 );
+        throw runtime_error( str );
+    }
+}
+// Function used to check that of 'for_what' is specified, then 'required_option' is specified too.
+void option_dependency(const variables_map &_vm, const char *_for_what, const char *_required_option)
+{
+    if ( _vm.count(_for_what) && !_vm[_for_what].defaulted() && ( !_vm.count(_required_option) || _vm[_required_option].defaulted() ) )
+    {
+        string str("Command line parameter \"%1\" must be used with \"%2\""); // TODO: Move this message to the resource file
+        replace<string>( &str, "%1", _for_what );
+        replace<string>( &str, "%2", _required_option );
+        throw runtime_error( str );
     }
 }
 
@@ -47,15 +58,17 @@ typedef struct SOptions
 {
     typedef enum ECommand { Start, Stop } ECommand_t;
 
-    SOptions():       // Default options' values
+    SOptions():               // Default options' values
             m_Command(Start),
-            m_sPidfileDir("/tmp/")
+            m_sPidfileDir("/tmp/"),
+            m_bDaemonize(false)
     {}
 
     string m_sConfigFile;
     string m_sInstanceName;
     ECommand_t m_Command;
     string m_sPidfileDir;
+    bool m_bDaemonize;
 }
 SOptions_t;
 
@@ -72,8 +85,9 @@ bool ParseCmdLine( int _Argc, char *_Argv[], SOptions_t *_Options ) throw (excep
     ("config,c", value<string>(), "configuration file")
     ("start", "start PROOFAgent daemon (default action)")
     ("stop", "stop PROOFAgent daemon")
-    ("instance,i", value<string>(), "name of the instance of daemon")
+    ("instance,i", value<string>(), "name of the instance of PROOFAgent")
     ("pidfile,p", value<string>(), "directory where daemon can keep its pid file. (Default: /tmp/)") // TODO: I am thinking to move this option to config file
+    ("daemonize,d", "run PROOFAgent as a daemon")
     ;
 
     // Parsing command-line
@@ -81,16 +95,18 @@ bool ParseCmdLine( int _Argc, char *_Argv[], SOptions_t *_Options ) throw (excep
     store(parse_command_line(_Argc, _Argv, desc), vm);
     notify(vm);
 
-    if ( vm.empty() )
-        throw logic_error( "PROOFAgent: No command line arguments were given" );
-
-    conflicting_options(vm, "start", "stop");
-
-    if ( vm.count("help") )
+    if ( vm.count("help") || vm.empty() )
     {
         cout << desc << endl;
         return false;
     }
+
+    conflicting_options( vm, "start", "stop" );
+    option_dependency(vm, "start", "daemonize" );
+    option_dependency(vm, "stop", "daemonize" );
+    option_dependency(vm, "pidfile", "daemonize" );
+    option_dependency(vm, "start", "pidfile" );
+
     if ( vm.count("config") )
         _Options->m_sConfigFile = vm["config"].as<string>();
     if ( vm.count("start") )
@@ -105,8 +121,7 @@ bool ParseCmdLine( int _Argc, char *_Argv[], SOptions_t *_Options ) throw (excep
         // We need to be sure that there is "/" always at the end of the path
         smart_append<string>( &_Options->m_sPidfileDir, '/' );
     }
-    else
-        throw logic_error( "PROOFAgent: option \"pidfile\" is mandatory" );
+    _Options->m_bDaemonize = vm.count("daemonize");
 
     return true;
 }
@@ -168,55 +183,64 @@ int main( int argc, char *argv[] )
         return erOK;
     }
 
-    // process ID and Session ID
-    pid_t pid;
-    pid_t sid;
+    if ( Options.m_bDaemonize )
+    {
+        // process ID and Session ID
+        pid_t pid;
+        pid_t sid;
 
-    // Fork off the parent process
-    pid = ::fork();
-    if ( pid < 0 )
-        return ( erError );
+        // Fork off the parent process
+        pid = ::fork();
+        if ( pid < 0 )
+            return ( erError );
 
-    // If we got a good PID, then we can exit the parent process.
-    if ( pid > 0 )
-        return ( erOK );
+        // If we got a good PID, then we can exit the parent process.
+        if ( pid > 0 )
+            return ( erOK );
 
-    // Change the file mode mask
-    ::umask( 0 );
+        // Change the file mode mask
+        ::umask( 0 );
 
-    // Create a new SID for the child process
-    sid = ::setsid();
-    if ( sid < 0 )  // TODO:  Log the failure
-        return ( erError );
-
+        // Create a new SID for the child process
+        sid = ::setsid();
+        if ( sid < 0 )  // TODO:  Log the failure
+            return ( erError );
+    }
     // Main object - agent itself
     CPROOFAgent agent;
 
     try
     {
-        CPIDFile pidfile( pidfile_name.str(), ::getpid() );
+        if ( Options.m_bDaemonize )
+        {
+            CPIDFile pidfile( pidfile_name.str(), ::getpid() );
+        }
 
         // Daemon-specific initialization goes here
         if ( FAILED( agent.ReadCfg( Options.m_sConfigFile, Options.m_sInstanceName ) ) )
             return erError; // TODO: Log me!
 
-        // Change the current working directory
-        //     chdir("/") to ensure that our process doesn't keep any directory
-        //     in use. Failure to do this could make it so that an administrator
-        //     couldn't unmount a file system, because it was our current directory.
-        if ( ::chdir( "/" ) < 0 ) // TODO: Log the failure
-            return ( erError );
+        int fd;
+        if ( Options.m_bDaemonize )
+        {
+            // Change the current working directory
+            //     chdir("/") to ensure that our process doesn't keep any directory
+            //     in use. Failure to do this could make it so that an administrator
+            //     couldn't unmount a file system, because it was our current directory.
+            if ( ::chdir( "/" ) < 0 ) // TODO: Log the failure
+                return ( erError );
 
-        // Close out the standard file descriptors
-        close( STDIN_FILENO );
-        close( STDOUT_FILENO );
-        close( STDERR_FILENO );
+            // Close out the standard file descriptors
+            close( STDIN_FILENO );
+            close( STDOUT_FILENO );
+            close( STDERR_FILENO );
 
-        // Establish new open descriptors for stdin, stdout, and stderr. Even if
-        //  we don't plan to use them, it is still a good idea to have them open.
-        int fd = open("/dev/null", O_RDWR); // stdin - file handle 0.
-        dup(fd);                        // stdout - file handle 1.
-        dup(fd);                        // stderr - file handle 2.
+            // Establish new open descriptors for stdin, stdout, and stderr. Even if
+            //  we don't plan to use them, it is still a good idea to have them open.
+            fd = open("/dev/null", O_RDWR); // stdin - file handle 0.
+            dup(fd);                        // stdout - file handle 1.
+            dup(fd);                        // stderr - file handle 2.
+        }
 
         // Starting Agent
         agent.Start();
