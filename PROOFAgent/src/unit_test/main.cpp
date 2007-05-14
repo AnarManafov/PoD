54bfcaf5611ebc08b19c8f109c030490860dfb97
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-05-14 09:26:54 +0000

    added: unit-tests for MiscUtils.h
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/PROOFAgent/trunk@787 47ffe4a8-6206-0410-815e-8f101de37557

--- PROOFAgent/src/unit_test/main.cpp
+++ PROOFAgent/src/unit_test/main.cpp
@@ -1,55 +1,151 @@
 /************************************************************************/
 /**
- * @file $$File name$$
- * @brief $$File comment$$
+ * @file Test_MiscUtils.cpp
+ * @brief Unit tests of MiscCommon
  * @author Anar Manafov A.Manafov@gsi.de
- *//*
-
-        version number:    $LastChangedRevision$
-        created by:        Anar Manafov
-                           $$date$$
+ */ /*
+ 
+        version number:   $LastChangedRevision$
+        created by:          Anar Manafov
+                                  2007-03-10
         last changed by:   $LastChangedBy$ $LastChangedDate$
-
+ 
         Copyright (c) 2006,2007 GSI GridTeam. All rights reserved.
-*************************************************************************/
+*************************************************************************/ 
 // BOOST: tests
 #include <boost/test/unit_test.hpp>
 using boost::unit_test::test_suite;
 
+// STD
+#include <string>
+
 // Our
-#include "INet.h"
+#include "MiscUtils.h"
 
-using namespace MiscCommon::INet;
+using namespace MiscCommon;
+using namespace std;
 
-void test_smart_counter();
+void test_smart_append();
+void test_replace();
+void test_to_lower();
+void test_to_upper();
 
-        
+
 test_suite* init_unit_test_suite( int, char* [] )
 {
-   test_suite * test = BOOST_TEST_SUITE( "Unit tests of PROOFAgent" );
+    test_suite * test = BOOST_TEST_SUITE( "Unit tests of MiscCommon" );
 
-   // test->add( BOOST_TEST_CASE( &tf_smart_socket ), 0 );
+    test->add( BOOST_TEST_CASE( &test_smart_append ), 0 );
+    test->add( BOOST_TEST_CASE( &test_replace ), 0 );
+    test->add( BOOST_TEST_CASE( &test_to_lower ), 0 );
+    test->add( BOOST_TEST_CASE( &test_to_upper ), 0 );
 
     return test;
 }
 
 
 
-void test_smart_counter()
+void test_smart_append()
 {
-    /*{
-        smart_socket s1;
-        BOOST_CHECK( s1.count() == 0 );
+    const string sTempl("/Test1/Test/");
+    const string sTempl2("Test1/Test/");
+
+    {
+        string sVal = "/Test1/Test/";
+        smart_append( &sVal, '/' );
+        BOOST_CHECK( sTempl == sVal );
     }
 
     {
-        smart_socket s2( AF_INET, SOCK_STREAM, 0 );
-        BOOST_CHECK( s1.count() == 0 );
-        int Socket = s2;
-        s2.add_ref();
-            ::close(Socket);
-        BOOST_CHECK( s2.count() == 0 );
-    }*/
+        string sVal = "/Test1/Test";
+        smart_append( &sVal, '/' );
+        BOOST_CHECK( sTempl == sVal );
+    }
+
+    {
+        string sVal = "Test1/Test";
+        smart_append( &sVal, '/' );
+        BOOST_CHECK( sTempl2 == sVal );
+    }
 }
 
+void test_replace()
+{
+    const string sTempl("Test_HELLO/Tset");
 
+    {
+        string sVal = "%1_HELLO/Tset";
+        replace<string>( &sVal, "%1", "Test");
+        BOOST_CHECK( sTempl == sVal );
+    }
+
+    {
+        string sVal = "Test_HELLO/%1";
+        replace<string>( &sVal, "%1", "Tset");
+        BOOST_CHECK( sTempl == sVal );
+    }
+
+    {
+        string sVal = "Test_%1/Tset";
+        replace<string>( &sVal, "%1", "HELLO");
+        BOOST_CHECK( sTempl == sVal );
+    }
+
+}
+
+void test_to_lower()
+{
+    const string sTempl("test_4hello");
+
+    {
+        string sVal = "TesT_4HEllO";
+        to_lower( sVal );
+        BOOST_CHECK( sTempl == sVal );
+    }
+}
+
+void test_to_upper()
+{
+    const string sTempl("TEST2_HELLO");
+
+    {
+        string sVal = "TesT2_HEllo";
+        to_upper( sVal );
+        BOOST_CHECK( sTempl == sVal );
+    }
+}
+
+void test_trim()
+{
+    const string sTempl("TEST2_HELLO");
+
+    {
+        string sVal = " TEST2_HELLO ";
+        trim<string>( &sVal, " " );
+        BOOST_CHECK( sTempl == sVal );
+    }
+
+    {
+        string sVal = "/TEST2_HELLO";
+        trim<string>( &sVal, "/" );
+        BOOST_CHECK( sTempl == sVal );
+    }
+
+    {
+        string sVal = "TEST2_HELLO/";
+        trim<string>( &sVal, "/" );
+        BOOST_CHECK( sTempl == sVal );
+    }
+
+    {
+        string sVal = "TEST2_HELLOO";
+        trim<string>( &sVal, "O" );
+        BOOST_CHECK( sTempl == sVal );
+    }
+
+    {
+        string sVal = "HELLOTEST2_HELLOHELLO";
+        trim<string>( &sVal, "HELLO" );
+        BOOST_CHECK( sTempl == sVal );
+    }
+}
