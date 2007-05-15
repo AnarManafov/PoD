571c72145883b5892495995739b987c85576e0df
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-05-15 09:41:19 +0000

    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/gLite_PROOF/trunk@798 47ffe4a8-6206-0410-815e-8f101de37557

---
+++ gLite_PROOF/gLitePROOF.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+#/************************************************************************/
+#/**
+# * @file gLitePROOF.sh
+# * @brief $$File comment$$
+# * @author Anar Manafov A.Manafov@gsi.de
+# *//*
+#
+#        version number:    $LastChangedRevision$
+#        created by:        Anar Manafov
+#                           2007-05-15
+#        last changed by:   $LastChangedBy$ $LastChangedDate$
+#
+#        Copyright (c) 2007 GSI GridTeam. All rights reserved.
+#*************************************************************************/
+
+
