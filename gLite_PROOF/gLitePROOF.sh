5832fef6ddd5f7779be79a639d4ef22c39a393b5
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-05-15 10:23:03 +0000

    changed: developing a gLite job package
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/gLite_PROOF/trunk@801 47ffe4a8-6206-0410-815e-8f101de37557

--- gLite_PROOF/gLitePROOF.sh
+++ gLite_PROOF/gLitePROOF.sh
@@ -15,4 +15,42 @@
 #        Copyright (c) 2007 GSI GridTeam. All rights reserved.
 #*************************************************************************/
 
+# ROOT
+export ROOTSYS=/usr/ROOT/5.14.00
+export PATH=$ROOTSYS/bin:$PATH
+export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
 
+
+# start xrootd
+echo "Starting xrootd..."
+xrootd -c ~/xpd.cf -b -l ~/xpd.log
+
+#
+#
+#RET_VAL=$?
+#if [ "X$RET_VAL" = "X0" ]; then
+#  echo "successful. Exit code: $RET_VAL"
+#  
+#else
+#  echo "unsuccessful. Exit code: $RET_VAL"
+#fi
+#
+#
+
+# start proofagent
+./proofagent -i client -c ~/proofagent.cfg.xml --start
+RET_VAL=$?
+if [ "X$RET_VAL" = "X0" ]; then
+  echo "proofagent successful. Exit code: $RET_VAL"
+  
+else
+  echo "cant start proofagent. Exit code: $RET_VAL"
+fi
+
+#
+# daemon mode
+#./proofagent -d -i client1 -p /tmp/ -c proofagent.cfg.xml --start
+#
+
+# sleep - in order to keep a job slot
+#sleep 1200 # 1200 seconds
