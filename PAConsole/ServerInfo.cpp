1c022ff81ae0d3b8b6f2a5fe64183c53963c315d
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-05-29 11:06:11 +0000

    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/PAConsole/trunk@836 47ffe4a8-6206-0410-815e-8f101de37557

---
+++ PAConsole/ServerInfo.cpp
@@ -0,0 +1,23 @@
+/************************************************************************/
+/**
+ * @file ServerInfo.cpp
+ * @brief Implementation
+ * @author Anar Manafov A.Manafov@gsi.de
+ *//*
+
+        version number:    $LastChangedRevision$
+        created by:        Anar Manafov
+                           $$date$$
+        last changed by:   $LastChangedBy$ $LastChangedDate$
+
+        Copyright (c) 2007 GSI GridTeam. All rights reserved.
+*************************************************************************/
+#include "ServerInfo.h"
+
+ServerInfo::ServerInfo()
+{
+}
+
+ServerInfo::~ServerInfo()
+{
+}
---
+++ PAConsole/ServerInfo.h
@@ -0,0 +1,25 @@
+/************************************************************************/
+/**
+ * @file ServerInfo.h
+ * @brief Interface
+ * @author Anar Manafov A.Manafov@gsi.de
+ *//*
+
+        version number:    $LastChangedRevision$
+        created by:        Anar Manafov
+                           $$date$$
+        last changed by:   $LastChangedBy$ $LastChangedDate$
+
+        Copyright (c) 2007 GSI GridTeam. All rights reserved.
+*************************************************************************/
+#ifndef SERVERINFO_H_
+#define SERVERINFO_H_
+
+class ServerInfo
+{
+public:
+	ServerInfo();
+	virtual ~ServerInfo();
+};
+
+#endif /*SERVERINFO_H_*/
