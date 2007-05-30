0b771e1028b0f73deb8d51cf4f556c7b976aa82e
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-05-30 15:42:29 +0000

    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/gLite_PROOF/trunk@844 47ffe4a8-6206-0410-815e-8f101de37557

--- gLite_PROOF/myselector.C
+++ gLite_PROOF/myselector.C
@@ -30,74 +30,73 @@
 
 void myselector::Begin(TTree *tree)
 {
-   // The Begin() function is called at the start of the query.
-   // When running with PROOF Begin() is only called on the client.
-   // The tree argument is deprecated (on PROOF 0 is passed).
-
-   TString option = GetOption();
+  // The Begin() function is called at the start of the query.
+  // When running with PROOF Begin() is only called on the client.
+  // The tree argument is deprecated (on PROOF 0 is passed).
+  
+  TString option = GetOption();
 }
 
 void myselector::SlaveBegin(TTree *tree)
 {
-   // The SlaveBegin() function is called after the Begin() function.
-   // When running with PROOF SlaveBegin() is called on each slave server.
-   // The tree argument is deprecated (on PROOF 0 is passed).
-
-   Init(tree);
-
-   TString option = GetOption();
-   fHist = new TH1F("fHist","ptd0_d",40,0.0,8.0);
+  // The SlaveBegin() function is called after the Begin() function.
+  // When running with PROOF SlaveBegin() is called on each slave server.
+  // The tree argument is deprecated (on PROOF 0 is passed).
+  
+  Init(tree);
+  
+  TString option = GetOption();
+  fHist = new TH1F("fHist","ptd0_d",40,0.0,8.0);
 }
 
 Bool_t myselector::Process(Long64_t entry)
 {
-   // The Process() function is called for each entry in the tree (or possibly
-   // keyed object in the case of PROOF) to be processed. The entry argument
-   // specifies which entry in the currently loaded tree is to be processed.
-   // It can be passed to either TTree::GetEntry() or TBranch::GetEntry()
-   // to read either all or the required parts of the data. When processing
-   // keyed objects with PROOF, the object is already loaded and is available
-   // via the fObject pointer.
-   //
-   // This function should contain the "body" of the analysis. It can contain
-   // simple or elaborate selection criteria, run algorithms on the data
-   // of the event and typically fill histograms.
-
-   // WARNING when a selector is used with a TChain, you must use
-   //  the pointer to the current TTree to call GetEntry(entry).
-   //  The entry is always the local entry number in the current tree.
-   //  Assuming that fChain is the pointer to the TChain being processed,
-   //  use fChain->GetTree()->GetEntry(entry).
-   fChain->GetTree()->GetEntry(entry);
-   fHist->Fill(ptd0_d);
-   return kTRUE;
+  // The Process() function is called for each entry in the tree (or possibly
+  // keyed object in the case of PROOF) to be processed. The entry argument
+  // specifies which entry in the currently loaded tree is to be processed.
+  // It can be passed to either TTree::GetEntry() or TBranch::GetEntry()
+  // to read either all or the required parts of the data. When processing
+  // keyed objects with PROOF, the object is already loaded and is available
+  // via the fObject pointer.
+  //
+  // This function should contain the "body" of the analysis. It can contain
+  // simple or elaborate selection criteria, run algorithms on the data
+  // of the event and typically fill histograms.
+  
+  // WARNING when a selector is used with a TChain, you must use
+  //  the pointer to the current TTree to call GetEntry(entry).
+  //  The entry is always the local entry number in the current tree.
+  //  Assuming that fChain is the pointer to the TChain being processed,
+  //  use fChain->GetTree()->GetEntry(entry).
+  fChain->GetTree()->GetEntry(entry);
+  fHist->Fill(ptd0_d);
+  return kTRUE;
 }
 
 void myselector::SlaveTerminate()
 {
-   // The SlaveTerminate() function is called after all entries or objects
-   // have been processed. When running with PROOF SlaveTerminate() is called
-   // on each slave server.
-   fOutput->Add(fHist);
+  // The SlaveTerminate() function is called after all entries or objects
+  // have been processed. When running with PROOF SlaveTerminate() is called
+  // on each slave server.
+  fOutput->Add(fHist);
 }
 
 void myselector::Terminate()
 {
-   // The Terminate() function is the last function to be called during
-   // a query. It always runs on the client, it can be used to present
-   // the results graphically or save the results to file.#
-   cout << "DEBUG: myselector::Terminate()" << endl;
-   TIter next(fOutput);
-   TObject *obj;
-   while( (obj=next()) )
-   {
-       cout << "DEBUG: myselector::Terminate(): obj loop" << endl;
-       obj->Print();
-	const string sCName(obj->ClassName());
-	cout << "DEBUG: myselector::Terminate(): class: " << sCName << endl;
-	if(sCName == "TH1F")
-	       obj->Draw();
-   }
-   cout << "DEBUG: end of myselector::Terminate()" << endl;
+  // The Terminate() function is the last function to be called during
+  // a query. It always runs on the client, it can be used to present
+  // the results graphically or save the results to file.#
+  cout << "DEBUG: myselector::Terminate()" << endl;
+  TIter next(fOutput);
+  TObject *obj;
+  while( (obj=next()) )
+    {
+      cout << "DEBUG: myselector::Terminate(): obj loop" << endl;
+      obj->Print();
+      const string sCName(obj->ClassName());
+      cout << "DEBUG: myselector::Terminate(): class: " << sCName << endl;
+      if(sCName == "TH1F")
+	obj->Draw();
+    }
+  cout << "DEBUG: end of myselector::Terminate()" << endl;
 }
-
