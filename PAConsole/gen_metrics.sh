dc4dfa50d0ef67f1898527c1302d18d8b377d160
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-06-28 13:27:32 +0000

    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/PAConsole/trunk@974 47ffe4a8-6206-0410-815e-8f101de37557

--- PAConsole/gen_metrics.sh
+++ PAConsole/gen_metrics.sh
@@ -13,12 +13,12 @@ echo ">>> pre-processing svn.log file... <<<"
 sed -i "s/\/D-Grid\/PAConsole\/tags\//\/tags\//g" svn.log
 
 echo ">>> executing StatSVN... <<<"
-java -jar trunk/PAConsole/sbin/statsvn.jar -css ./trunk/PAConsole/sbin/statsvn.css -output-dir ./PAConsole-metrics -tags '.*' svn.log .
+java -jar ./PAConsole/trunk/PAConsole/sbin/statsvn.jar -css ./PAConsole/trunk/PAConsole/sbin/statsvn.css -output-dir ./PAConsole-metrics -tags '.*' svn.log ./PAConsole/
 
 
 echo ">>> generating SLOC count report... <<<"
-sloccount --crossdups  --wide ./trunk/PAConsole/ > result.txt
-./trunk/PAConsole/sbin/sloc2html.py result.txt > ./PAConsole-metrics/result.html
+sloccount --crossdups  --wide ./PAConsole/trunk/PAConsole/ > result.txt
+./PAConsole/trunk/PAConsole/sbin/sloc2html.py result.txt > ./PAConsole-metrics/result.html
 
 echo ">>> publishing metrics on the web... <<<"
 chmod og+rx -R PAConsole-metrics
