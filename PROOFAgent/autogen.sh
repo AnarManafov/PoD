d2f06d9e6e8b24adac3bcfe47a49db746152e017
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-07-24 17:59:52 +0000

    changed: autogen.sh - trying to make it a bit more user friednly
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/PROOFAgent/trunk@1087 47ffe4a8-6206-0410-815e-8f101de37557

--- PROOFAgent/autogen.sh
+++ PROOFAgent/autogen.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
 
+rm -fr autom4te.cache
+
 echo ">>> Producing a ChangeLog file... <<<"
 ./Make_ChangeLog.sh
 
-autoreconf -v --force --install -I config -I m4
+# Equivalent to
+#       aclocal
+#       autoheader
+#       automake --add-missing --copy
+#       autoconf
+autoreconf --verbose --force --install -Wall -I config -I m4 || exit 1
+
+echo "Now run ./configure and then make."
+exit 0
 
