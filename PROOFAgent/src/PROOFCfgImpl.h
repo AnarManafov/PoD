86c1cf8a22a1d53c954e62b5f876ec742b57edde
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-08-09 15:41:49 +0000

    PROOFAgent_modified: file header has been fixed
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/PROOFAgent/trunk@1173 47ffe4a8-6206-0410-815e-8f101de37557

--- PROOFAgent/src/PROOFCfgImpl.h
+++ PROOFAgent/src/PROOFCfgImpl.h
@@ -5,10 +5,10 @@
  * @author Anar Manafov A.Manafov@gsi.de
  */ /*
 
-        version number:     $LastChangedRevision: 938 $
+        version number:     $LastChangedRevision$
         created by:         Anar Manafov
                             2007-06-27
-        last changed by:    $LastChangedBy: manafov $ $LastChangedDate: 2007-06-26 19:51:54 +0200 (Tue, 26 Jun 2007) $
+        last changed by:    $LastChangedBy$ $LastChangedDate$
 
         Copyright (c) 2007 GSI GridTeam. All rights reserved.
 *************************************************************************/
