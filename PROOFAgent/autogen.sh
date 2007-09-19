1c7b517fe82f3721052957ad414a78c19ac45e84
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-09-19 16:32:11 +0000

    PROOFAgent: checking existence of ChangeLog during autogen execution
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/PROOFAgent/trunk@1289 47ffe4a8-6206-0410-815e-8f101de37557

--- PROOFAgent/autogen.sh
+++ PROOFAgent/autogen.sh
@@ -2,12 +2,17 @@
 
 rm -fr autom4te.cache
 
+# Checking that ChangeLog is exist
+if [ ! -e "ChangeLog" ]; then
+ ./Make_ChangeLog.sh || touch "ChangeLog"
+fi
+
 # Equivalent to
 #       aclocal
 #       autoheader
 #       automake --add-missing --copy
 #       autoconf
-autoreconf --verbose --force --install -Wall -I config -I m4 || exit 1
+autoreconf --verbose --force --install -I config -I m4 || exit 1
 
 echo "Now run ./configure and then make."
 exit 0
