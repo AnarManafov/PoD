5b7e4740beb04257bc7f84de9c943ab985afd457
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-11-08 14:10:46 +0000

    PROOFAgent_deleted: unit_tests of MiscCommon
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/PROOFAgent/trunk@1351 47ffe4a8-6206-0410-815e-8f101de37557

--- PROOFAgent/unit_tests.sh
+++ PROOFAgent/unit_tests.sh
@@ -2,7 +2,7 @@
 
 source proofagent_env.sh
 
-$PROOFAGENT_BIN/UT_MiscUtils || exit 1
-$PROOFAGENT_BIN/UT_Process || exit 1
+#$PROOFAGENT_BIN/UT_MiscUtils || exit 1
+#$PROOFAGENT_BIN/UT_Process || exit 1
 
 exit 0
