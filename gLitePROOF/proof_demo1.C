3d59718055226955e7c56806b4572ddd082ee1bf
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-11-26 13:16:40 +0000

    gLitePROOF_modified: This version has been tested with FZK WMS.
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/gLitePROOF/trunk@1439 47ffe4a8-6206-0410-815e-8f101de37557

--- gLitePROOF/proof_demo1.C
+++ gLitePROOF/proof_demo1.C
@@ -19,8 +19,8 @@
   TDSet *set = new TDSet( "TTree","h42" );
   
   // Files to analyze 
-  set->Add( "http://www-linux.gsi.de/~manafov/D-Grid/Release/ROOT_DATA/dstarmb.root" );
-  set->Add( "http://www-linux.gsi.de/~manafov/D-Grid/Release/ROOT_DATA/dstarmb2.root" );
+  set->Add( "root://depc218.gsi.de:20094//tmp/dstarmb.root" );
+  set->Add( "root://depc218.gsi.de:20094//tmp/dstarmb2.root" );
   
   // connecting to local PROOF server
   proof = TProof::Open( "localhost" );
