ded1e22dee6aa44f93876dce356c91c9e02c9121
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-12-05 13:03:01 +0000

    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/gLitePROOF/trunk@1478 47ffe4a8-6206-0410-815e-8f101de37557

--- gLitePROOF/make_disk.sh
+++ gLitePROOF/make_disk.sh
@@ -1,10 +1,28 @@
 #! /bin/bash
 
+VERSION=2.0.2
+
 mkdir -p gLitePROOFpackage || exit 1
 
-cp  --target-directory=gLitePROOFpackage -rv documentation gLitePROOF.jdl gLitePROOF.sh myselector.C myselector.h PAConsole proofagent proofagent-1.0.2.1270.tar.gz proofagent.cfg.xml proof_demo1.C READ.ME Server_gLitePROOF.sh xpd.cf || exit 1
+cp  --target-directory=gLitePROOFpackage -rv \
+	PAConsole \
+	READ.ME  \
+	Server_gLitePROOF.sh \
+	documentation \
+	dstarmb.root \
+	gLitePROOF.jdl \
+	gLitePROOF.sh \
+	gLitePROOF_FZK.jdl \
+	glite-api-wrapper.cfg.xml \
+	myselector.C \
+	myselector.h \
+	proof_demo1.C \
+	proofagent \
+	proofagent.cfg.xml \
+	xpd.cf \
+	|| exit 1
 
-tar  -czvf gLitePROOFpackage.2.0.1.tgz gLitePROOFpackage || exit 1
+tar  -czvf gLitePROOFpackage.$VERSION.tgz gLitePROOFpackage || exit 1
 
 rm -rf gLitePROOFpackage || exit 1
 
