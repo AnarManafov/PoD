3bbdf78584e6a4d460a76ec34cf856d90840dabb
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2008-01-28 16:55:56 +0000

    gLitePROOF_modifed: make_dist has been updated
    git-svn-id: https://subversion.gsi.de/dgrid/gLitePROOF/trunk@1703 47ffe4a8-6206-0410-815e-8f101de37557

--- gLitePROOF/make_disk.sh
+++ gLitePROOF/make_disk.sh
@@ -1,26 +1,26 @@
 #! /bin/bash
 
-VERSION=2.0.2
+VERSION=2.0.3
 
 mkdir -p gLitePROOFpackage || exit 1
 
 cp  --target-directory=gLitePROOFpackage -rv \
-	PAConsole \
-	READ.ME  \
-	Server_gLitePROOF.sh \
-	documentation \
-	dstarmb.root \
-	gLitePROOF.jdl \
-	gLitePROOF.sh \
 	gLitePROOF_FZK.jdl \
-	glite-api-wrapper.cfg.xml \
 	myselector.C \
+	proofagent.cfg.xml \
+	xpd.cf.template \
+	gLitePROOF.jdl \
 	myselector.h \
 	proof_demo1.C \
+	documentation \
+	gLitePROOF.sh \
+	PAConsole \
+	READ.ME \
+	dstarmb.root \
 	proofagent \
-	proofagent.cfg.xml \
-	xpd.cf \
+	Server_gLitePROOF.sh \
 	|| exit 1
+
 
 tar  -czvf gLitePROOFpackage.$VERSION.tgz gLitePROOFpackage || exit 1
 
