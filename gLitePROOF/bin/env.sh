b1f9de239c91bf84012c53124289807e78e83bd0
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2008-02-12 13:17:45 +0000

    git-svn-id: https://subversion.gsi.de/dgrid/gLitePROOF/trunk@1717 47ffe4a8-6206-0410-815e-8f101de37557

--- gLitePROOF/bin/env.sh
+++ gLitePROOF/bin/env.sh
@@ -2,4 +2,3 @@
 
 export GLITE_PROOF_LOCATION=/home/anar/svn-dgrid/dgrid/gLitePROOF/trunk/gLitePROOF
 export PATH=$GLITE_PROOF_LOCATION/bin:$PATH
-export LD_LIBRARY_PATH=$GLITE_PROOF_LOCATION/lib:$LD_LIBRARY_PATH
