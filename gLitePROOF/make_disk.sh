af371b2ec853c6ace22d1f0230ff8c23ea44cdf7
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2008-02-24 16:18:43 +0000

    gLitePROOF_modified: added a revision number to the name of the package
    git-svn-id: https://subversion.gsi.de/dgrid/gLitePROOF/trunk@1775 47ffe4a8-6206-0410-815e-8f101de37557

--- gLitePROOF/make_disk.sh
+++ gLitePROOF/make_disk.sh
@@ -11,7 +11,6 @@ gmake || exit 1
 popd
 
 # making pkg.
-
 mkdir -p $PKG_NAME || exit 1
 
 cp --target-directory=$PKG_NAME -rv \
@@ -29,8 +28,12 @@ rm -rf $PKG_NAME/etc/.svn
 rm -rf $PKG_NAME/template/.svn
 rm -rf $PKG_NAME/test/.svn
 
-tar  -cvf $PKG_NAME.$VERSION.tar $PKG_NAME || exit 1
-gzip -9 $PKG_NAME.$VERSION.tar || exit 1
+# a revision number
+REV=`svn info https://subversion.gsi.de/dgrid/gLitePROOF/trunk/gLitePROOF  | grep "Revision: " | head -1 | awk -F": " '{printf("%s", $ 2)}'`
+echo "REV=$REV"
+THE_NAME=$PKG_NAME.$VERSION.$REV
+tar  -cvf $THE_NAME.tar $PKG_NAME || exit 1
+gzip -9 $THE_NAME.tar || exit 1
 
 
 rm -rf $PKG_NAME || exit 1
