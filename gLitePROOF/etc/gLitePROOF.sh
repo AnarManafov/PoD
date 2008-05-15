688332e30bbb8f8234fab168c91f25ee38e56b25
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2008-05-15 19:52:37 +0000

    gLitePROOF_modified: improving grid job's script [Trac Ticket #52]
    git-svn-id: https://subversion.gsi.de/dgrid/gLitePROOF/trunk@1826 47ffe4a8-6206-0410-815e-8f101de37557

--- gLitePROOF/etc/gLitePROOF.sh
+++ gLitePROOF/etc/gLitePROOF.sh
@@ -12,8 +12,34 @@
 #                           2007-05-15
 #        last changed by:   $LastChangedBy$ $LastChangedDate$
 #
-#        Copyright (c) 2007 GSI GridTeam. All rights reserved.
+#        Copyright (c) 2007-2008 GSI GridTeam. All rights reserved.
 #*************************************************************************/
+
+# ************************************************************************
+# ***** Perform program exit housekeeping *****
+function clean_up {
+# Killing all xrootd in anyway (TODO: must be removed, needs an elegant solution)
+    pkill -9 xrootd
+    pkill -9 proofserv
+    
+# Removing local proof directory
+# assuming that proof directory is located in the user's home
+    proof_dir="${HOME}/proof"
+    if [ -e "$proof_dir" ]; then
+	echo "$proof_dir exists and will be deleted..."
+	rm -rf $proof_dir
+    fi
+    
+    exit $1
+}
+# ************************************************************************
+
+
+# ************************************************************************
+# ***** Main *****
+
+# handle signals
+trap clean_up SIGHUP SIGINT SIGTERM 
 
 # current working dir
 WD=`pwd`
@@ -30,60 +56,61 @@ _TMP_DIR=`mktemp -d /tmp/gLitePROOF_XXXXXXXXXX`
 eval sed -i 's%_G_WORKER_TMP_DIR%$_TMP_DIR%g' ./xpd.cf
 
 # host's CPU/instruction set
-   host_arch=`( uname -p ) 2>&1`
-   case "$host_arch" in
-   i386|sparc|ppc|alpha|arm|mips)
-     ;;
-   powerpc) # Darwin returns 'powerpc'
-     host_arch=ppc
-     ;;
-   *) # uname -p on Linux returns 'unknown' for the processor type,
+host_arch=`( uname -p ) 2>&1`
+case "$host_arch" in
+    i386|sparc|ppc|alpha|arm|mips)
+	;;
+    powerpc) # Darwin returns 'powerpc'
+	host_arch=ppc
+	;;
+    *) # uname -p on Linux returns 'unknown' for the processor type,
       # OpenBSD returns 'Intel Pentium/MMX ("Genuine Intel" 586-class)'
-
+	
       # Maybe uname -m (machine hardware name) returns something we
       # recognize.
-
+	
       # x86/x86pc is used by QNX
-      case "`( uname -m ) 2>&1`" in
-      i[3-9]86*|x86|x86pc|k5|k6|k6_2|k6_3|k6-2|k6-3|pentium*|athlon*|i586_i686|i586-i686) host_arch=x86 ;;
-      ia64) host_arch=ia64 ;;
-      x86_64) host_arch=x86_64 ;;
-      ppc) host_arch=ppc ;;
-      alpha) host_arch=alpha ;;
-      sparc*) host_arch=sparc ;;
-      9000*) host_arch=hppa ;;
-      arm*) host_arch=arm ;;
-      s390) host_arch=s390 ;;
-      s390x) host_arch=s390x ;;
-      mips) host_arch=mips ;;
-      *) host_arch=UNKNOWN ;;
-    esac
-    ;;
-  esac
+	case "`( uname -m ) 2>&1`" in
+	    i[3-9]86*|x86|x86pc|k5|k6|k6_2|k6_3|k6-2|k6-3|pentium*|athlon*|i586_i686|i586-i686) host_arch=x86 ;;
+	    ia64) host_arch=ia64 ;;
+	    x86_64) host_arch=x86_64 ;;
+	    ppc) host_arch=ppc ;;
+	    alpha) host_arch=alpha ;;
+	    sparc*) host_arch=sparc ;;
+	    9000*) host_arch=hppa ;;
+	    arm*) host_arch=arm ;;
+	    s390) host_arch=s390 ;;
+	    s390x) host_arch=s390x ;;
+	    mips) host_arch=mips ;;
+	    *) host_arch=UNKNOWN ;;
+	esac
+	;;
+esac
 
 echo "*** host's CPU/instruction set: " $host_arch
 
 case "$host_arch" in
-x86)
+    x86)
 	PROOFAGENT_ARC="proofagent-1_0_4_1692-x86-linux-gcc_3_4.tar.gz"
 	ROOT_ARC="root_v5.18.00.Linux.slc4.gcc3.4.tar.gz" ;;
-x86_64)
+    x86_64)
         PROOFAGENT_ARC="proofagent-1_0_4_1692-x86_64-linux-gcc_3_4.tar.gz"
         ROOT_ARC="root_v5.18.00.Linux.slc4_amd64.gcc3.4.tar.gz" ;;
 esac
 
-# ROOT
-wget --tries=2 http://www-linux.gsi.de/~manafov/D-Grid/Release/Binaries/$ROOT_ARC || exit 1
-tar -xzvf $ROOT_ARC || exit 1
+# ****************
+# ***** ROOT *****
+wget --tries=2 http://www-linux.gsi.de/~manafov/D-Grid/Release/Binaries/$ROOT_ARC || clean_up 1
+tar -xzvf $ROOT_ARC || clean_up 1
 
 export ROOTSYS="/$WD/root"
 export PATH=$ROOTSYS/bin:$PATH
 export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
 
-
-# PROOFAgent
-wget --tries=2 http://www-linux.gsi.de/~manafov/D-Grid/Release/Binaries/$PROOFAGENT_ARC  || exit 1
-tar -xzf $PROOFAGENT_ARC || exit 1
+# **********************
+# ***** PROOFAgent *****
+wget --tries=2 http://www-linux.gsi.de/~manafov/D-Grid/Release/Binaries/$PROOFAGENT_ARC  || clean_up 1
+tar -xzf $PROOFAGENT_ARC || clean_up 1
 
 export PROOFAGENTSYS="/$WD/proofagent"
 export PATH=$PROOFAGENTSYS/:$PATH 
@@ -109,34 +136,18 @@ if [ "X$XRD_RET_VAL" = "X0" ]; then
     echo "XROOTD successful."
 else
     echo "problem to start xrootd. Exit code: $XRD_RET_VAL"
-    exit 1
+    clean_up 1
 fi
 
 # start proofagent
 proofagent -i client -c $WD/proofagent.cfg.xml
 RET_VAL=$?
 if [ "X$RET_VAL" = "X0" ]; then
-    echo "proofagent successful. Exit code: $RET_VAL"    
+    echo "proofagent successful. Exit code: $RET_VAL"
 else
     echo "cant start proofagent. Exit code: $RET_VAL"
+    clean_up 1
 fi
 
-#
-# daemon mode
-#./proofagent -d -i client1 -p /tmp/ -c proofagent.cfg.xml --start
-#
-
-# sleep - in order to keep a job slot
-#sleep 1200 # 1200 seconds
-
-# Killing all xrootd in anyway (TODO: must be removed, needs an elegant solution)
-pkill -9 xrootd
-pkill -9 proofserv
-
-# Removing local proof directory
-# assuming that proof directory is located in the user's home
-proof_dir="${HOME}/proof"
-if [ -e "$proof_dir" ]; then
-    echo "$proof_dir exists and will be deleted..."
-    rm -rf $proof_dir
-fi
+# Exit
+clean_up 0
