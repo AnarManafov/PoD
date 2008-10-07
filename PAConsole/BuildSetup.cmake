7038f0180ffa27b3b68478de059b876d75a4c67b
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2008-10-07 18:09:36 +0000

    PAConsole: moving the project to cmake buil system
    git-svn-id: https://subversion.gsi.de/dgrid/PAConsole/trunk@2006 47ffe4a8-6206-0410-815e-8f101de37557

---
+++ PAConsole/BuildSetup.cmake
@@ -0,0 +1,16 @@
+#
+# to build vsual-netstat:
+#  1) mkdir build
+#  2) cd build
+#  3) cmake -C ../BuildSetup.cmake ..
+#  4) gmake install
+#
+
+#
+# General Options
+#
+
+#
+# Documentation
+#
+SET( BUILD_DOCUMENTATION ON CACHE BOOL "Build source code documentation" FORCE )
