146831fc6704ed9115bc7280a1a5c1e9eb13eb3e
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2008-12-09 14:33:41 +0000

    PAConsole: added a template for LSF jobmanager plug-in
    git-svn-id: https://subversion.gsi.de/dgrid/PAConsole/trunk@2057 47ffe4a8-6206-0410-815e-8f101de37557

---
+++ PAConsole/plugins/LSFPlugin/src/LSFDlg.cpp
@@ -0,0 +1,402 @@
+/************************************************************************/
+/**
+ * @file $$File name$$
+ * @brief $$File comment$$
+ * @author Anar Manafov A.Manafov@gsi.de
+ *//*
+
+        version number:    $LastChangedRevision$
+        created by:        Anar Manafov
+                           2008-12-09
+        last changed by:   $LastChangedBy$ $LastChangedDate$
+
+        Copyright (c) 2008 GSI GridTeam. All rights reserved.
+ *************************************************************************/
+// Qt
+#include <QtGui>
+// BOOST
+//#include <boost/archive/xml_oarchive.hpp>
+//#include <boost/archive/xml_iarchive.hpp>
+// PAConsole LSF plug-in
+#include "LSFDlg.h"
+
+using namespace std;
+
+CLSFDlg::CLSFDlg( QWidget *parent ) :
+        QWidget( parent ),
+        m_JobsCount( 0 )
+{
+    m_ui.setupUi( this );
+
+    m_Timer = new QTimer( this );
+    connect( m_Timer, SIGNAL( timeout() ), this, SLOT( updateJobsTree() ) );
+
+//    connect( &m_JobSubmitter, SIGNAL( changeProgress( int ) ), this, SLOT( setProgress( int ) ) );
+//    connect( &m_JobSubmitter, SIGNAL( sendThreadMsg( const QString& ) ), this, SLOT( recieveThreadMsg( const QString& ) ) );
+//    connect( &m_JobSubmitter, SIGNAL( changeNumberOfJobs( int ) ), this, SLOT( setNumberOfJobs( int ) ) );
+
+    createActions();
+
+    clipboard = QApplication::clipboard();
+
+//    // Set completion for the edit box of JDL file name
+//    QCompleter *completer = new QCompleter( this );
+//    completer->setModel( new QDirModel( completer ) );
+//    m_ui.edtJDLFileName->setCompleter( completer );
+//
+//    try
+//    {
+//        // Loading class from the config file
+//        _loadcfg( *this, g_szCfgFileName );
+//    }
+//    catch ( ... )
+//    {
+//        setAllDefault();
+//    }
+}
+
+CLSFDlg::~CLSFDlg()
+{
+//    try
+//    {
+//        // Saving class to the config file
+//        _savecfg( *this, g_szCfgFileName );
+//    }
+//    catch ( ... )
+//    {
+//    }
+
+    if ( m_Timer )
+    {
+        m_Timer->stop();
+        delete m_Timer;
+    }
+}
+
+void CLSFDlg::setAllDefault()
+{
+//    m_JDLFileName = g_szDefaultJDL;
+//    m_JobSubmitter.setAllDefault();
+//    UpdateAfterLoad();
+}
+
+void CLSFDlg::UpdateAfterLoad()
+{
+//    smart_path( &m_JDLFileName );
+//    m_ui.edtJDLFileName->setText( m_JDLFileName.c_str() );
+//
+//    // Setting up PARAMETERS
+//    int num_jobs( 0 );
+//    try
+//    {
+//        get_ad_attr( &num_jobs, m_JDLFileName, JDL_PARAMETERS );
+//
+//        updateJobsTree();
+//    }
+//    catch ( ... )
+//    {
+//    }
+//    m_ui.spinNumWorkers->setValue( num_jobs );
+}
+
+void CLSFDlg::recieveThreadMsg( const QString &_Msg )
+{
+//    QMessageBox::critical( this, tr( "PROOFAgent Console" ), _Msg );
+//    m_ui.btnSubmitClient->setEnabled( true );
+}
+
+void CLSFDlg::on_btnSubmitClient_clicked()
+{
+//    // Checking first that gLitePROOF server is running
+//    CServerInfo si;
+//    if ( !si.IsRunning( true ) )
+//    {
+//        const string msg( "gLitePROOF server is not running.\n"
+//                          "Do you want to submit this job anyway?" );
+//        const QMessageBox::StandardButton reply =
+//            QMessageBox::question( this, tr( "PROOFAgent Console" ), tr( msg.c_str() ),
+//                                   QMessageBox::Yes | QMessageBox::No );
+//        if ( QMessageBox::Yes != reply )
+//            return;
+//    }
+//
+//    if ( !m_JobSubmitter.isRunning() )
+//    {
+//        if ( !QFileInfo( m_ui.edtJDLFileName->text() ).exists() )
+//        {
+//            QMessageBox::critical( this,
+//                                   tr( "PROOFAgent Console" ),
+//                                   tr( "File\n\"%1\"\ndoesn't exist!" ).arg(
+//                                       m_ui.edtJDLFileName->text() ) );
+//            return;
+//        }
+//
+//        m_JDLFileName = m_ui.edtJDLFileName->text().toAscii().data();
+//
+//        // Setting up PARAMETERS
+//        set_ad_attr( m_ui.spinNumWorkers->value(), m_JDLFileName, JDL_PARAMETERS );
+//
+//        m_JobSubmitter.setJDLFileName( m_JDLFileName );
+//        m_JobSubmitter.setEndpoint( m_ui.cmbEndpoint->currentText().toAscii().data() );
+//        // submit gLite jobs
+//        m_JobSubmitter.start();
+//        m_ui.btnSubmitClient->setEnabled( false );
+//    }
+//    else
+//    {
+//        // Job submitter's thread
+//        m_JobSubmitter.terminate();
+//        setProgress( 0 );
+//        m_ui.btnSubmitClient->setEnabled( true );
+//    }
+}
+
+void CLSFDlg::updateJobsTree()
+{
+//    m_TreeItems.update( m_JobSubmitter.getActiveJobList(), m_ui.treeJobs );
+}
+
+void CLSFDlg::on_btnBrowseJDL_clicked()
+{
+//    const QString dir = QFileInfo( m_ui.edtJDLFileName->text() ).absolutePath();
+//    const QString filename = QFileDialog::getOpenFileName( this, tr( "Select a jdl file" ), dir,
+//                                                           tr( "JDL Files (*.jdl)" ) );
+//    if ( QFileInfo( filename ).exists() )
+//    {
+//        m_JDLFileName = filename.toAscii().data();
+//        m_ui.edtJDLFileName->setText( filename );
+//    }
+}
+
+void CLSFDlg::createActions()
+{
+//    // COPY Job ID
+//    copyJobIDAct = new QAction( tr( "&Copy JobID" ), this );
+//    copyJobIDAct->setShortcut( tr( "Ctrl+C" ) );
+//    copyJobIDAct->setStatusTip( tr( "Copy selected jod id to the clipboard" ) );
+//    connect( copyJobIDAct, SIGNAL( triggered() ), this, SLOT( copyJobID() ) );
+//    // CANCEL Job
+//    cancelJobAct = new QAction( tr( "Canc&el Job" ), this );
+//    cancelJobAct->setShortcut( tr( "Ctrl+E" ) );
+//    cancelJobAct->setStatusTip( tr( "Cancel the selected jod" ) );
+//    connect( cancelJobAct, SIGNAL( triggered() ), this, SLOT( cancelJob() ) );
+//    // GET OUTPUT of the Job
+//    getJobOutputAct = new QAction( tr( "Get &output" ), this );
+//    getJobOutputAct->setShortcut( tr( "Ctrl+O" ) );
+//    getJobOutputAct->setStatusTip( tr( "Get output sandbox of the selected jod" ) );
+//    connect( getJobOutputAct, SIGNAL( triggered() ), this, SLOT( getJobOutput() ) );
+//    // Get Logging Info
+//    getJobLoggingInfoAct = new QAction( tr( "Get &logging info" ), this );
+//    getJobLoggingInfoAct->setShortcut( tr( "Ctrl+L" ) );
+//    getJobLoggingInfoAct->setStatusTip( tr( "Get logging info of the selected jod" ) );
+//    connect( getJobLoggingInfoAct, SIGNAL( triggered() ), this, SLOT( getJobLoggingInfo() ) );
+//    // Remove Job from monitoring
+//    removeJobAct = new QAction( tr( "&Remove Job" ), this );
+//    removeJobAct->setShortcut( tr( "Ctrl+R" ) );
+//    removeJobAct->setStatusTip( tr( "Remove the selected job from monitoring" ) );
+//    connect( removeJobAct, SIGNAL( triggered() ), this, SLOT( removeJob() ) );
+}
+
+void CLSFDlg::contextMenuEvent( QContextMenuEvent *event )
+{
+//    // Checking that *treeJobs* has been selected
+//    QPoint pos = event->globalPos();
+//    if ( !m_ui.treeJobs->childrenRect().contains( m_ui.treeJobs->mapFromGlobal( pos ) ) )
+//        return;
+//
+//    // We need to disable menu items when no jobID is selected
+//    const QTreeWidgetItem * item( m_ui.treeJobs->currentItem() );
+//
+//    QMenu menu( this );
+//    menu.addAction( copyJobIDAct );
+//    copyJobIDAct->setEnabled( item );
+//
+//    menu.addAction( getJobOutputAct );
+//    getJobOutputAct->setEnabled( item );
+//
+//    menu.addAction( getJobLoggingInfoAct );
+//    getJobLoggingInfoAct->setEnabled( item );
+//
+//    menu.addSeparator();
+//    menu.addAction( removeJobAct );
+//    removeJobAct->setEnabled( item );
+//
+//    menu.addSeparator();
+//    menu.addAction( cancelJobAct );
+//    cancelJobAct->setEnabled( item );
+//
+//    menu.exec( event->globalPos() );
+}
+
+void CLSFDlg::copyJobID() const
+{
+//    // Copy selected JobID to clipboard
+//    const QTreeWidgetItem *item( m_ui.treeJobs->currentItem() );
+//    if ( !item )
+//        return;
+//    clipboard->setText( item->text( 0 ), QClipboard::Clipboard );
+//    clipboard->setText( item->text( 0 ), QClipboard::Selection );
+}
+
+void CLSFDlg::cancelJob()
+{
+//    const QTreeWidgetItem *item( m_ui.treeJobs->currentItem() );
+//    if ( !item )
+//        return;
+//
+//    const string jobid( item->text( 0 ).toAscii().data() );
+//    const string msg( "Do you really want to cancel the job:\n" + jobid );
+//    const QMessageBox::StandardButton
+//    reply( QMessageBox::question( this, tr( "PROOFAgent Console" ), tr( msg.c_str() ),
+//                                  QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel ) );
+//    if ( QMessageBox::Yes != reply )
+//        return;
+//
+//    try
+//    {
+//        CGLiteAPIWrapper::Instance().GetJobManager().JobCancel( jobid );
+//    }
+//    catch ( const exception &_e )
+//    {
+//        QMessageBox::critical( this, tr( "PROOFAgent Console" ), tr( _e.what() ) );
+//        return;
+//    }
+}
+
+void CLSFDlg::getJobOutput()
+{
+//    const QTreeWidgetItem *item( m_ui.treeJobs->currentItem() );
+//    if ( !item )
+//        return;
+//
+//    bool ok;
+//    const QString
+//    path =
+//        QInputDialog::getText(
+//            this,
+//            tr( "PROOFAgent Console" ),
+//            tr(
+//                "Enter the path, where output files should be delivered to:" ),
+//            QLineEdit::Normal, QDir::home().absolutePath(), &ok );
+//    if ( !ok || path.isEmpty() )
+//        return;
+//
+//    const string jobid( item->text( 0 ).toAscii().data() );
+//
+//    gaw_path_type joboutput_path;
+//    try
+//    {
+//        CGLiteAPIWrapper::Instance().GetJobManager().JobOutput( jobid, path.toAscii().data(),
+//                                                                &joboutput_path, true );
+//    }
+//    catch ( const exception &_e )
+//    {
+//        QMessageBox::critical( this, tr( "PROOFAgent Console" ), tr( _e.what() ) );
+//        return;
+//    }
+//
+//    ostringstream ss;
+//    transform( joboutput_path.begin(), joboutput_path.end(),
+//               ostream_iterator<string> ( ss, "\n____\n" ), gaw_path_type_to_string );
+//    QMessageBox::information( this, tr( "PROOFAgent Console" ), tr( ss.str().c_str() ) );
+}
+
+void CLSFDlg::getJobLoggingInfo()
+{
+//    // Job ID
+//    const QTreeWidgetItem *item( m_ui.treeJobs->currentItem() );
+//    if ( !item )
+//        return;
+//
+//    const string jobid( item->text( 0 ).toAscii().data() );
+//
+//    CLogInfoDlg dlg( this, jobid );
+//    dlg.exec();
+}
+
+void CLSFDlg::removeJob()
+{
+//    // Job ID
+//    const QTreeWidgetItem *item( m_ui.treeJobs->currentItem() );
+//    if ( !item )
+//        return;
+//
+//    const string jobid( item->text( 0 ).toAscii().data() );
+//
+//    m_JobSubmitter.RemoveJob( jobid );
+//    updateJobsTree();
+}
+
+void CLSFDlg::setProgress( int _Val )
+{
+//    if ( 100 == _Val )
+//    {
+//        m_ui.btnSubmitClient->setEnabled( true );
+//        // The job is submitted, we need to update the tree
+//        updateJobsTree();
+//    }
+//    m_ui.progressSubmittedJobs->setValue( _Val );
+}
+
+// Retrieving a list of possible WMProxy endpoints
+void CLSFDlg::UpdateEndpoints( bool _Msg )
+{
+//    m_ui.cmbEndpoint->clear();
+//
+//    CWMPEndpoint endpoint;
+//    StringVector_t endpoints;
+//    try
+//    {
+//        endpoint.Get( &endpoints, m_JDLFileName );
+//    }
+//    catch ( const exception &_e )
+//    {
+//        if ( _Msg )
+//            QMessageBox::critical( this, tr( "PROOFAgent Console" ), tr( _e.what() ) );
+//        return;
+//    }
+//
+//    // Let's fill the Combobox
+//    StringVector_t::const_iterator iter( endpoints.begin() );
+//    StringVector_t::const_iterator iter_end( endpoints.end() );
+//    for ( ; iter != iter_end; ++iter )
+//    {
+//        m_ui.cmbEndpoint->addItem( tr( iter->c_str() ) );
+//    }
+}
+
+void CLSFDlg::on_edtJDLFileName_textChanged( const QString &/*_text*/ )
+{
+//    m_JDLFileName = m_ui.edtJDLFileName->text().toAscii().data();
+//    UpdateEndpoints( false );
+}
+
+void CLSFDlg::setNumberOfJobs( int _count )
+{
+//    m_JobsCount = _count;
+//    emit changeNumberOfJobs( _count );
+}
+
+QString CLSFDlg::getName() const
+{
+    return QString( "LSF\nJob Manager" );
+}
+QWidget* CLSFDlg::getWidget()
+{
+    return this;
+}
+QIcon CLSFDlg::getIcon()
+{
+    return QIcon( ":/images/lsf.png" );
+}
+void CLSFDlg::startUpdTimer( int _JobStatusUpdInterval )
+{
+    // start or restart the timer
+    m_Timer->start( _JobStatusUpdInterval * 1000 );
+}
+int CLSFDlg::getJobsCount() const
+{
+    return m_JobsCount;
+}
+
+Q_EXPORT_PLUGIN2( LSFJobManager, CLSFDlg );
---
+++ PAConsole/plugins/LSFPlugin/src/LSFDlg.h
@@ -0,0 +1,104 @@
+/************************************************************************/
+/**
+ * @file $$File name$$
+ * @brief $$File comment$$
+ * @author Anar Manafov A.Manafov@gsi.de
+ *//*
+
+        version number:    $LastChangedRevision$
+        created by:        Anar Manafov
+                           2008-12-09
+        last changed by:   $LastChangedBy$ $LastChangedDate$
+
+        Copyright (c) 2008 GSI GridTeam. All rights reserved.
+*************************************************************************/
+#ifndef LSFDLG_H_
+#define LSFDLG_H_
+
+// Qt autogen. file
+#include "ui_wgLSF.h"
+// PAConsole
+#include "IJobManager.h"
+
+
+class CLSFDlg: public QWidget, IJobManager
+{
+        Q_OBJECT
+        Q_INTERFACES( IJobManager )
+
+    public:
+        CLSFDlg( QWidget *parent = NULL );
+        virtual ~CLSFDlg();
+
+    public:
+        // IJobManager interface
+        QString getName() const;
+        QWidget *getWidget();
+        QIcon getIcon();
+        void startUpdTimer( int _JobStatusUpdInterval );
+        int getJobsCount() const;
+
+        void setAllDefault();
+
+    signals:
+        void changeNumberOfJobs( int _count );
+
+    public slots:
+        void on_btnSubmitClient_clicked();
+        void updateJobsTree();
+        void recieveThreadMsg( const QString &_Msg );
+        void setProgress( int _Val );
+        void on_btnBrowseJDL_clicked();
+        void on_edtJDLFileName_textChanged( const QString & /*_text*/ );
+        void setNumberOfJobs( int _count );
+
+
+    private slots:
+        void copyJobID() const;
+        void cancelJob();
+        void getJobOutput();
+        void getJobLoggingInfo();
+        void removeJob();
+
+    protected:
+        void contextMenuEvent( QContextMenuEvent *event );
+
+    private:
+        void createActions();
+        void UpdateEndpoints( bool _Msg = true );
+        void UpdateAfterLoad();
+
+//        // serialization
+//        template<class Archive>
+//        void save( Archive & _ar, const unsigned int /*_version*/ ) const
+//        {
+//            _ar
+//            & BOOST_SERIALIZATION_NVP( m_JDLFileName )
+//            & BOOST_SERIALIZATION_NVP( m_JobSubmitter );
+//        }
+//        template<class Archive>
+//        void load( Archive & _ar, const unsigned int /*_version*/ )
+//        {
+//            _ar
+//            & BOOST_SERIALIZATION_NVP( m_JDLFileName )
+//            & BOOST_SERIALIZATION_NVP( m_JobSubmitter );
+//            UpdateAfterLoad();
+//        }
+//        BOOST_SERIALIZATION_SPLIT_MEMBER()
+
+    private:
+        Ui::wgGrid m_ui;
+        QTimer *m_Timer;
+        QAction *copyJobIDAct;
+        QAction *cancelJobAct;
+        QAction *getJobOutputAct;
+        QAction *getJobLoggingInfoAct;
+        QAction *removeJobAct;
+        QClipboard *clipboard;
+        std::string m_JDLFileName;
+        int m_JobsCount;
+};
+
+//BOOST_CLASS_VERSION( CGridDlg, 1 )
+
+#endif /*LSFDLG_H_*/
