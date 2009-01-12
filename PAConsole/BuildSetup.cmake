75fa128f0ec8e54967b628f12d43045d4ec993d5
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2009-01-12 11:18:10 +0000

    PAConsole: fixed windows layout
    git-svn-id: https://subversion.gsi.de/dgrid/PAConsole/trunk@2088 47ffe4a8-6206-0410-815e-8f101de37557

--- PAConsole/BuildSetup.cmake
+++ PAConsole/BuildSetup.cmake
@@ -17,7 +17,7 @@ endif ( NOT $ENV{GLITE_PROOF_LOCATION} STREQUAL "" )
 
 # set cmake build type, default value is: RelWithDebInfo
 # possible options are: None Debug Release RelWithDebInfo MinSizeRel
-set( CMAKE_BUILD_TYPE Debug CACHE STRING "Choose the type of build" FORCE )
+set( CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build" FORCE )
 #set(CMAKE_VERBOSE_MAKEFILE TRUE CACHE BOOL "This is useful for debugging only." FORCE)
 
 # This is needed if you want to use gLite plug-in and have several version of BOOST installed
@@ -36,4 +36,4 @@ set( BUILD_DOCUMENTATION ON CACHE BOOL "Build source code documentation" FORCE )
 set( BUILD_GLITE_PLUGIN ON CACHE BOOL "Build source code documentation" FORCE )
 
 # LSF plug-in
-set( LSF_PREFIX "/home/anar/LSF" CACHE STRING "LSF prefix" FORCE) 
+#set( LSF_PREFIX "/home/anar/LSF" CACHE STRING "LSF prefix" FORCE) 
