22011489f1689494aebeafc98978aee7c8fe72f8
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2009-04-01 13:09:01 +0000

    using xnsdoc instead of xsddoc
    git-svn-id: https://subversion.gsi.de/dgrid/PROOFAgent/trunk@2232 47ffe4a8-6206-0410-815e-8f101de37557

--- PROOFAgent/document_xsd.sh
+++ PROOFAgent/document_xsd.sh
@@ -3,5 +3,5 @@
 FOLDER="documentation/PROOFAgent_config/PROOFAgent_cfg_XML_Schema"
 rm -rf $FOLDER
 mkdir $FOLDER
-xsddoc -t "XML Schema for the PROOFAgent configuration file" -o $FOLDER -verbose documentation/PROOFAgent_config/proofagent.cfg.xsd
+xnsdoc -t "XML Schema for the PROOFAgent configuration file" -o $FOLDER -verbose documentation/PROOFAgent_config/proofagent.cfg.xsd
 
