9d09ec9628cc639f68edf867155a45504cff9ed4
Anar Manafov <anar@localhost.localdomain>
2009-05-13 16:02:33 +0200

    removed unused code.

--- src/ServerDlg.cpp
+++ src/ServerDlg.cpp
@@ -14,7 +14,6 @@
 *************************************************************************/
 // Qt
 #include <QWidget>
-#include <QtXml/QDomDocument>
 #include <QMessageBox>
 #include <QFile>
 #include <QFileDialog>
@@ -31,9 +30,8 @@
 #include "ServerInfo.h"
 
 // TODO: optimize the call of the status of PoD
-// this is very expensive call, we therefore using 30 sec. timeout
+// this is very expensive call, we therefore using 20 sec. timeout
 const size_t g_UpdateInterval = 20000;  // in milliseconds
-const char * const g_szPROOF_CFG = "$GLITE_PROOF_LOCATION/etc/proofagent.cfg.xml";
 // default pid/log directory
 const char * const g_szPID_Dir = "$GLITE_PROOF_LOCATION/";
 
@@ -46,9 +44,6 @@ CServerDlg::CServerDlg( QWidget *_parent ):
         m_PIDDir( g_szPID_Dir )
 {
     m_ui.setupUi( this );
-
-    // PROOFAgent server's Port number
-    //getSrvPort( &m_SrvPort );
 
     // pid/log directory
     smart_path( &m_PIDDir );
@@ -103,12 +98,12 @@ bool CServerDlg::IsRunning( bool _check_all )
 
 void CServerDlg::on_btnStartServer_clicked()
 {
-	CommandServer( srvSTART );
+    CommandServer( srvSTART );
 }
 
 void CServerDlg::on_btnStopServer_clicked()
 {
-	CommandServer( srvSTOP );
+    CommandServer( srvSTOP );
 }
 
 void CServerDlg::on_btnBrowsePIDDir_clicked()
@@ -135,73 +130,10 @@ void CServerDlg::update_check_srv_socket()
     string output;
     try
     {
-    	do_execv( cmd, params, 20, &output );
+        do_execv( cmd, params, 20, &output );
     }
-    catch(...)
+    catch (...)
     {
     }
     m_ui.edtServerInfo->setText( QString(output.c_str()) );
-}
-
-void CServerDlg::getSrvPort( int *_Port )
-{
-    if ( !_Port )
-        return ;
-
-    string cfg( g_szPROOF_CFG );
-    smart_path( &cfg );
-
-    QFile file( cfg.c_str() );
-    if ( !file.open( QFile::ReadOnly | QFile::Text ) )
-    {
-        QMessageBox::warning( this, tr( "PROOFAgent Console" ),
-                              tr( "Cannot read file %1:\n%2." )
-                              .arg( cfg.c_str() )
-                              .arg( file.errorString() ) );
-        return ;
-    }
-
-    QDomDocument domDocument;
-    QString errorStr;
-    int errorLine;
-    int errorColumn;
-
-    if ( !domDocument.setContent( &file, true, &errorStr, &errorLine, &errorColumn ) )
-    {
-        QMessageBox::information( window(), tr( "PROOFAgent Console" ),
-                                  tr( "Parse error at line %1, column %2:\n%3" )
-                                  .arg( errorLine )
-                                  .arg( errorColumn )
-                                  .arg( errorStr ) );
-        return ;
-    }
-    QDomNodeList instance = domDocument.elementsByTagName( "instance" );
-    if ( instance.isEmpty() )
-        return; // TODO: msg me!
-
-    QDomNode server;
-    for ( int i = 0; i < instance.count(); ++i )
-    {
-        const QDomNode name(
-            instance.item( i ).attributes().namedItem( "name" ) );
-        if ( name.isNull() )
-            continue;
-        // TODO: We look exactly for "server" instance!
-        // Change it. Move instance name to the PAConsole settings.
-        if ( name.nodeValue() == "server" )
-        {
-            server = instance.item( i );
-            break;
-        }
-    }
-
-    QDomNode agent_server = server.namedItem( "agent_server" );
-    if ( agent_server.isNull() )
-        return ; // TODO: Msg me!
-
-    QDomNode port = agent_server.namedItem( "listen_port" ).firstChild();
-    if ( port.isNull() )
-        return ; // TODO: Msg me!
-
-    *_Port = port.nodeValue().toInt();
 }
--- src/ServerDlg.h
+++ src/ServerDlg.h
@@ -29,51 +29,50 @@ class QTimer;
 
 class CServerDlg: public QWidget
 {
-        Q_OBJECT
+    Q_OBJECT
 
-        friend class boost::serialization::access;
+    friend class boost::serialization::access;
 
-    public:
-        CServerDlg( QWidget *_parent = NULL );
-        virtual ~CServerDlg();
+public:
+    CServerDlg( QWidget *_parent = NULL );
+    virtual ~CServerDlg();
 
-    public:
-	 enum EServerCommands{ srvSTART, srvSTOP };
+public:
+    enum EServerCommands{ srvSTART, srvSTOP };
 
-	 void CommandServer( EServerCommands _command );
+    void CommandServer( EServerCommands _command );
 
-    private slots:
-        void on_btnStartServer_clicked();
-        void on_btnStopServer_clicked();
-        void on_btnBrowsePIDDir_clicked();
+private slots:
+    void on_btnStartServer_clicked();
+    void on_btnStopServer_clicked();
+    void on_btnBrowsePIDDir_clicked();
 
-        void update_check_srv_socket();
+    void update_check_srv_socket();
 
-    private:
-        void getSrvPort( int *_Port );
-        bool IsRunning( bool _check_all );
+private:
+    bool IsRunning( bool _check_all );
 
-        template<class Archive>
-        void save( Archive & _ar, const unsigned int /*_version*/ ) const
-        {
-            _ar & BOOST_SERIALIZATION_NVP( m_PIDDir );
-        }
-        template<class Archive>
-        void load( Archive & _ar, const unsigned int /*_version*/ )
-        {
-            _ar & BOOST_SERIALIZATION_NVP( m_PIDDir );
+    template<class Archive>
+    void save( Archive & _ar, const unsigned int /*_version*/ ) const
+    {
+        _ar & BOOST_SERIALIZATION_NVP( m_PIDDir );
+    }
+    template<class Archive>
+    void load( Archive & _ar, const unsigned int /*_version*/ )
+    {
+        _ar & BOOST_SERIALIZATION_NVP( m_PIDDir );
 
-            // pid/log directory
-            MiscCommon::smart_path( &m_PIDDir );
-            m_ui.edtPIDDir->setText( m_PIDDir.c_str() );
-        }
-        BOOST_SERIALIZATION_SPLIT_MEMBER()
+        // pid/log directory
+        MiscCommon::smart_path( &m_PIDDir );
+        m_ui.edtPIDDir->setText( m_PIDDir.c_str() );
+    }
+    BOOST_SERIALIZATION_SPLIT_MEMBER()
 
-    private:
-        Ui::wgServer m_ui;
-        int m_SrvPort;
-        QTimer *m_Timer;
-        std::string m_PIDDir;
+private:
+    Ui::wgServer m_ui;
+    int m_SrvPort;
+    QTimer *m_Timer;
+    std::string m_PIDDir;
 };
 
 BOOST_CLASS_VERSION( CServerDlg, 1 )
