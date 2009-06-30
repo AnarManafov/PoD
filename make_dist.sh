dd123f1de747e3475388bb8d8a9c88d2a3cd0a6c
Anar Manafov <anar@localhost.localdomain>
2009-06-30 15:27:14 +0200

    starting PoD v2.0.9b

--- make_dist.sh
+++ make_dist.sh
@@ -10,7 +10,7 @@
 ##
 ##         Copyright (c) 2008-2009 GSI GridTeam. All rights reserved.
 ##*************************************************************************/
-VERSION=2.0.8
+VERSION=2.0.9b
 
 PKG_NAME=PoD_Package
 
