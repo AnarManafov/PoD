2f669f16b4d9eec53b11d8fc4a27e1a95830976e
Anar Manafov <anar@kp1nbg042.gsi.de>
2010-01-12 16:26:59 +0100

    added a License file + using Nightly for this build

--- make_dist.sh
+++ make_dist.sh
@@ -10,7 +10,7 @@
 ##
 ##         Copyright (c) 2008-2009 GSI GridTeam. All rights reserved.
 ##*************************************************************************/
-VERSION=2.1.3a
+VERSION=2.1.3b
 
 PKG_NAME=PoD_Package
 
