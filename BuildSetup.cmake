4ef36973d2d64ed4593338419a48e6b1175ee2cf
Anar Manafov <anar@kp1nbg042.gsi.de>
2010-05-14 15:41:39 +0200

    added glite-api-wrapper as a part of gLite plug-in - fixed

--- BuildSetup.cmake
+++ BuildSetup.cmake
@@ -6,39 +6,27 @@
 #  4) gmake install
 #
 
-#
-# General Options
-#
-
-# Install directory
-if ( NOT $ENV{POD_LOCATION} STREQUAL "" )
-set (CMAKE_INSTALL_PREFIX "$ENV{POD_LOCATION}" CACHE PATH "Install path prefix, prepended onto install directories." FORCE)
-endif ( NOT $ENV{POD_LOCATION} STREQUAL "" )
-
 # set cmake build type, default value is: RelWithDebInfo
 # possible options are: None Debug Release RelWithDebInfo MinSizeRel
 set( CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build" FORCE )
+
 #set(CMAKE_VERBOSE_MAKEFILE TRUE CACHE BOOL "This is useful for debugging only." FORCE)
 
 # This is needed if you want to use gLite plug-in and have several version of BOOST installed
 #set( Boost_USE_MULTITHREADED OFF CACHE BOOL "BOOST" FORCE )
 
-#
 # Documentation
-#
 set( BUILD_DOCUMENTATION ON CACHE BOOL "Build source code documentation" FORCE )
 
-#
 # Build TESTS
-#
 set( BUILD_TESTS ON CACHE BOOL "Build pod-console unit tests" FORCE )
 
-#
-# Plug-ins
-#
-
 # gLite plug-in
-set( BUILD_GLITE_PLUGIN ON CACHE BOOL "Build source code documentation" FORCE )
+#set( BUILD_GLITE_PLUGIN OFF CACHE BOOL "Build source code documentation" FORCE )
 
 # LSF plug-in
-#set( LSF_PREFIX "/home/anar/LSF" CACHE STRING "LSF prefix" FORCE) 
+#set( BUILD_LSF_PLUGIN OFF CACHE BOOL "Build source code documentation" FORCE )
+#set( LSF_PREFIX "/home/anar/LSF" CACHE STRING "LSF prefix" FORCE)
+
+# PBS plug-in
+#set( BUILD_PBS_PLUGIN OFF CACHE BOOL "Build source code documentation" FORCE )
