84316fa2ab20b7b2b8796484bd3ed9a32916c105
Anar Manafov <anar@kp1nbg042.gsi.de>
2010-05-17 18:04:51 +0200

    init the project

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/************************************************************************/
+/**
+ * @file main.cpp
+ * @brief Implementation of the "Main" function
+ * @author Anar Manafov A.Manafov@gsi.de
+ */ /*
+
+        version number:     $LastChangedRevision$
+        created by:         Anar Manafov
+                            2010-05-17
+        last changed by:    $LastChangedBy$ $LastChangedDate$
+
+        Copyright (c) 2010 GSI, Scientific Computing group. All rights reserved.
+*************************************************************************/
