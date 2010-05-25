fd20837530a3953c5d4ef126dba46ee12e2af570
Anar Manafov <A.Manafov@gsi.de>
2010-05-25 15:16:28 +0200

    a test for gsi's source package name

--- bin/prep_gsi_bin.sh
+++ bin/prep_gsi_bin.sh
@@ -1,7 +1,20 @@
-#! /usr/bin/env bash
+! /usr/bin/env bash
 
 POD_SRC=$1
-POD_INST=$2
+
+# build PoD
+mkdir $POD_SRC/build
+mkdir $POD_SRC/inst_tmp
+# set a local install prefix
+export POD_LOCATION=$POD_SRC/inst_tmp
+
+pushd `pwd`
+cd $POD_SRC/build
+cmake -C ../BuildSetup.cmake ..
+make install
+popd
+
+POD_INST=$POD_LOCATION
 
 # Copy compiled parts of PoD
 cp -v $POD_INST/bin/* $POD_SRC/bin/ || exit 1
@@ -15,11 +28,18 @@ cp -v $POD_INST/plugins/* $POD_SRC/plugins/ || exit 1
 mkdir -p $POD_SRC/lib || exit 1
 cp -v /misc/manafov/PoD/forGSI/libs32b_fo_64bit/* $POD_SRC/lib/ || exit 1
 
-# make the source distr.
-cd $POD_SRC/build || exit 1
-make package_source
+echo "set( GSI_BUILD ON CACHE BOOL \"Build a GSI specific version\" FORCE )" >> $POD_SRC/BuildSetup.cmake || exit 1
 
-echo "set( GSI_BUILD ON CACHE BOOL "Build a GSI specific version" FORCE )" >> $POD_SRC/BuildSetup.cmake || exit 1
+# build a GSI specific distr.
+rm -rf $POD_SRC/build/*
+pushd `pwd`
+cd $POD_SRC/build
+cmake -C ../BuildSetup.cmake ..
+make package_source
+popd
+
+# release the tarball
+chmod go+xr $POD_SRC/*.tar.gz; scp -p $POD_SRC/*.tar.gz manafov@lxg0527:/misc/manafov/web-docs/D-Grid/Release/Nightly || exit 1
 
 exit 0
 
