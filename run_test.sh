aaf3950869f54c8273b9ef2a85b991d862c520a0
Anar Manafov <A.Manafov@gsi.de>
2010-06-17 16:00:18 +0200

    fix the unit-tests starter script

--- run_test.sh
+++ run_test.sh
@@ -9,19 +9,28 @@ clean_up()
 
 trap clean_up SIGHUP SIGINT SIGTERM
 
+
+exec_test() {
+
+if [ -e $1 ]; then
+	echo ">>> Processing $1"
+	./$1 || clean_up
+	echo
+else
+	echo "WARNING: can't find $1"
+fi
+
+}
+
 pushd `pwd`
 cd $1
 
 echo "----------------------"
 echo "pod-agent UNIT-TESTs"
 echo "----------------------"
-echo ">>> Processing tests of ProtocoleCommands"
-./pod-agent_test_ProtocolCommands || clean_up
-echo
-echo ">>> Processing tests of Protocole"
-./pod-agent_test_Protocol || clean_up
-echo
-echo ">>> Processing tests of ProofStatusFile"
+exec_test "pod-agent_test_ProtocolCommands"
+exec_test "pod-agent_test_Protocol"
+
 # prepare dummy files
 # TODO: implement a function for the following
 ADMIN_PATH_SRV="PoDServer/.xproofd.22222/activesessions"
@@ -32,13 +41,13 @@ echo "2" > $ADMIN_PATH_SRV/manafov.default.1234.status
 echo "0" > $ADMIN_PATH/manafov.default.1234.status
 echo "1" > $ADMIN_PATH/manafov.default.5678.status
 echo "3" > $ADMIN_PATH/manafov.default.9.status
-./pod-agent_test_ProofStatusFile || clean_up
+exec_test "pod-agent_test_ProofStatusFile"
+
 
 echo "----------------------"
 echo "pod-ssh UNIT-TESTs"
 echo "----------------------"
-echo ">>> Processing tests of the ssh-plugin config file engine"
-./pod-ssh_test_config
+exec_test "pod-ssh_test_config"
 
 popd
 
