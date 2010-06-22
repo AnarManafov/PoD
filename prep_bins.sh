35a49f5d6afcf94d6f7d653be85e924f9db10911
AnarManafov <manafov@lxi009.gsi.de>
2010-06-22 12:55:08 +0200

    make prep_bins.sh executable

--- prep_bins.sh
+++ prep_bins.sh
@@ -7,20 +7,24 @@ export LD_LIBRARY_PATH=/misc/manafov/Qt/4.4.2_etch32/lib:$LD_LIBRARY_PATH
 
 POD_SRC="/misc/manafov/PoD/BinBuilds"
 
-# prep repo
-pushd `pwd`
-cd $POD_SRC
-rm -rf $POD_SRC/PoD/*
-git clone ssh://anar@depc218.gsi.de//home/anar/GitRepository/PROOFonDemand/PoD || exit 1
-cd PoD
-git submodule update --init --recursive || exit 1
-popd
+ssh lxetch32 "cd $POD_SRC; git clone ssh://anar@depc218.gsi.de//home/anar/GitRepository/PROOFonDemand/PoD"
 
-# GSI bin
-ssh lxetch32 "$POD_SRC/PoD/bin/prep_gsi_bin.sh $POD_SRC/PoD" || exit 1
-
-# Workers bins
-ssh lxetch64 "$POD_SRC/PoD/bin/prep_wrk_bins.sh $POD_SRC/PoD" || exit 1
-ssh lxetch32 "$POD_SRC/PoD/bin/prep_wrk_bins.sh $POD_SRC/PoD" || exit 1
+if [ "$1" = "local" ]; then
+    ssh lxetch32 "cd $POD_SRC; rm -rf $POD_SRC/PoD/*; git clone ssh://anar@depc218.gsi.de//home/anar/GitRepository/PROOFonDemand/PoD"
+    ssh lxetch32 "$POD_SRC/PoD/prep_bins.sh"
+else
+    # prep repo
+    pushd `pwd`
+    cd $POD_SRC/PoD
+    git submodule update --init --recursive || exit 1
+    popd
+    
+    # GSI bin
+    ssh lxetch32 "$POD_SRC/PoD/bin/prep_gsi_bin.sh $POD_SRC/PoD" || exit 1
+    
+    # Workers bins
+    ssh lxetch64 "$POD_SRC/PoD/bin/prep_wrk_bins.sh $POD_SRC/PoD" || exit 1
+    ssh lxetch32 "$POD_SRC/PoD/bin/prep_wrk_bins.sh $POD_SRC/PoD" || exit 1
+fi
 
 exit 0
