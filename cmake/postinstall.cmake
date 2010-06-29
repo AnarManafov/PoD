3d7b74bec02f5bbaf9b2b072a12929c863d3800f
Anar Manafov <A.Manafov@gsi.de>
2010-06-29 13:41:40 +0200

    fix: the postinstall script can't see GSI_BUILD variable

--- cmake/postinstall.cmake
+++ cmake/postinstall.cmake
@@ -1,10 +1,9 @@
 
 # generate a default PoD configuration file
 
-# setting the lib path is important for GSI specific builds
-if(GSI_BUILD)
-	set( ENV{LD_LIBRARY_PATH} "${CMAKE_INSTALL_PREFIX}/lib:$ENV{LD_LIBRARY_PATH}" )
-endif(GSI_BUILD)
+# setting the lib path variable is important for GSI specific builds
+# we set it for all builds so-far, it doesn't break anything
+set( ENV{LD_LIBRARY_PATH} "${CMAKE_INSTALL_PREFIX}/lib:$ENV{LD_LIBRARY_PATH}" )
 # generate a default PoD configuration file
 execute_process(COMMAND ${CMAKE_INSTALL_PREFIX}/bin/pod-user-defaults -c ${CMAKE_INSTALL_PREFIX}/etc/PoD.cfg -d )
 
