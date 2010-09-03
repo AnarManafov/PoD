5e38a56d573851f891bc2cfc336810a1a3664c5b
Anar Manafov <A.Manafov@gsi.de>
2010-09-03 15:33:08 +0200

    move the creation of the default config from install to PoD_env.sh

--- cmake/postinstall.cmake
+++
@@ -1,9 +0,0 @@
-
-# generate a default PoD configuration file
-
-# setting the lib path variable is important for GSI specific builds
-# we set it for all builds so-far, it doesn't break anything
-set( ENV{LD_LIBRARY_PATH} "${CMAKE_INSTALL_PREFIX}/lib:$ENV{LD_LIBRARY_PATH}" )
-# generate a default PoD configuration file
-execute_process(COMMAND ${CMAKE_INSTALL_PREFIX}/bin/pod-user-defaults -c ${CMAKE_INSTALL_PREFIX}/etc/PoD.cfg -d )
-
