b5d956308ebfbf396ac75f79241aa305d3c60c95
Anar Manafov <Anar.Manafov@gmail.com>
2012-03-11 13:50:40 +0100

    implementing a working script with a payload - work in progress

--- etc/PoDWorker.sh
+++ etc/PoDWorker.sh
@@ -287,6 +287,30 @@ get_default_ROOT()
       clean_up 1
    fi
 }
+#=============================================================================
+# ***** untar_payload *****
+#
+#=============================================================================
+#uuencode=1
+#binary=0
+#
+#function untar_payload()
+#{
+#	match=$(grep --text --line-number '^PAYLOAD:$' $0 | cut -d ':' -f 1)
+#	payload_start=$((match + 1))
+#	if [[ $binary -ne 0 ]]; then
+#		tail -n +$payload_start $0 | tar -tzvf -
+#	fi
+#	if [[ $uuencode -ne 0 ]]; then
+#		tail -n +$payload_start $0 | uudecode | tar -tzvf -
+#	fi
+#}
+#
+#read -p "Install files? " ans
+#if [[ "${ans:0:1}"  ||  "${ans:0:1}" ]]; then
+#	untar_payload
+#	# Do remainder of install steps.
+#fi
 # ************************************************************************
 #
 # 				M A I N
